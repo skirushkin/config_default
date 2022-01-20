e378fb664be6869a56667a2fef918aa96b03f063
Stepan Kirushkin <stepan.kirushkin@gmail.com>
2022-01-20 18:26:30 +0300

    dev.

--- lib/config_default/init.rb
+++ lib/config_default/init.rb
@@ -5,8 +5,6 @@ module ConfigDefault::Init
 
   def init_rails_monkey_patch!
     return unless Object.const_defined?(:Rails)
-    return unless Object.const_defined?(:"Rails::Application")
-    return unless Object.const_defined?(:"Rails::Application::Configuration")
 
     Rails::Application.prepend(ConfigDefault::RailsApplicationExtension)
     Rails::Application::Configuration.prepend(ConfigDefault::RailsApplicationConfigurationExtension)
