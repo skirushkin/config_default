53a687680cc15c46508d8bd7d05b463ae9913b00
Stepan Kirushkin <stepan.kirushkin@gmail.com>
2026-08-12 12:56:50 +0300

    dev.

--- lib/config_default/rails_patch.rb
+++ lib/config_default/rails_patch.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 
-# NOTE: Patch Rails::Application to add support in #config_for method
-# https://github.com/rails/rails/blob/main/railties/lib/rails/application.rb#L295
-module ConfigDefault::RailsApplicationPatch
-  def config_for(name, env: Rails.env)
-    data = ConfigDefault.hash(name, key: env, deep_symbolize_keys: true)
-    ActiveSupport::OrderedOptions.new.merge(data)
-  end
-end
-
-# NOTE: Patch Rails::Application::Configuration to add support into database conf files
-# https://github.com/rails/rails/blob/main/railties/lib/rails/application/configuration.rb#L467
-module ConfigDefault::RailsApplicationConfigurationPatch
-  def load_database_yaml
-    ConfigDefault.hash(:database, key: nil)
+module ConfigDefault
+  # NOTE: Patch Rails::Application to add support in #config_for method
+  # https://github.com/rails/rails/blob/main/railties/lib/rails/application.rb#L295
+  class RailsApplicationPatch
+    def config_for(name, env: Rails.env)
+      data = ConfigDefault.hash(name, key: env, deep_symbolize_keys: true)
+      ActiveSupport::OrderedOptions.new.merge(data)
+    end
   end
 
-  def database_configuration
-    load_database_yaml
+  # NOTE: Patch Rails::Application::Configuration to add support into database conf files
+  # https://github.com/rails/rails/blob/main/railties/lib/rails/application/configuration.rb#L467
+  class RailsApplicationConfigurationPatch
+    def load_database_yaml
+      ConfigDefault.hash(:database, key: nil)
+    end
+
+    def database_configuration
+      load_database_yaml
+    end
   end
 end
