3b9a31cb83e8ba7a49fca0c551c5d4e3ecbad704
Stepan Kirushkin <stepan.kirushkin@gmail.com>
2026-08-12 12:59:39 +0300

    dev.

--- lib/config_default/rails_patch.rb
+++ lib/config_default/rails_patch.rb
@@ -3,7 +3,7 @@
 module ConfigDefault
   # NOTE: Patch Rails::Application to add support in #config_for method
   # https://github.com/rails/rails/blob/main/railties/lib/rails/application.rb#L295
-  class RailsApplicationPatch
+  module RailsApplicationPatch
     def config_for(name, env: Rails.env)
       data = ConfigDefault.hash(name, key: env, deep_symbolize_keys: true)
       ActiveSupport::OrderedOptions.new.merge(data)
@@ -12,7 +12,7 @@ module ConfigDefault
 
   # NOTE: Patch Rails::Application::Configuration to add support into database conf files
   # https://github.com/rails/rails/blob/main/railties/lib/rails/application/configuration.rb#L467
-  class RailsApplicationConfigurationPatch
+  module RailsApplicationConfigurationPatch
     def load_database_yaml
       ConfigDefault.hash(:database, key: nil)
     end
